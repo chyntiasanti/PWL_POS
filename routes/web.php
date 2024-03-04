b87d1f74690a5e0fdd9f146c81b8973fd57df2b9
chyntiasanti <chyntia.nurtris@gmail.com>
2024-03-04 09:21:13 +0700

    Praktikum Empat

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LevelController as ControllersLevelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
